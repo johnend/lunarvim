46d001f49d7b63545da99e9bcf059b9732b447f8
John Enderby <john.enderby.1987@gmail.com>
2024-05-06 23:41:55 +0100

    added local cat-mocha colors file, updated lualine and colorizer

--- lua/user/plugin_config/nvim-colorizer.lua
+++ lua/user/plugin_config/nvim-colorizer.lua
@@ -1,10 +1,15 @@
 require("colorizer").setup({
   filetypes = { "*" },
   user_default_options = {
-    css = true,
+    RGB = true,
+    RRGGBB = true,
+    RRGGBBAA = true,
     AARRGGBB = true,
     mode = "virtualtext",
     virtualtext = "■",
     always_update = true,
-  }
+  },
+  css = {css = true},
+  html = {css = true},
+  javascript = {css = true},
 })
